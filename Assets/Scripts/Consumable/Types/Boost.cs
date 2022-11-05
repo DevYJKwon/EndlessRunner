de4398fc261c57f9aa9f93e40494b6279bc23aae
dydwo2324@gmail.com <dydwo2324@gmail.com>
2022-11-06 03:48:44 +0900

    commit

---
+++ Assets/Scripts/Consumable/Types/Boost.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Boost : Consumable
+{
+
+    public override string GetConsumableName()
+    {
+        return "Boost";
+    }
+
+    public override ConsumableType GetConsumableType()
+    {
+        return ConsumableType.BOOST;
+    }
+
+    public override int GetPrice()
+    {
+        return 1;
+    }
+
+	public override int GetPremiumCost()
+	{
+		return 0;
+	}
+
+    public override IEnumerator Started(CharacterInputController c)
+    {
+        yield return base.Started(c);
+        //speed boost
+        c.trackManager.speed = c.trackManager.maxSpeed;
+    }
+
+    public override void Ended(CharacterInputController c)
+    {
+        base.Ended(c);
+        c.trackManager.speed = c.trackManager.minSpeed;
+    }
+}
